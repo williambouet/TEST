4fd881d5b9fbc798a9b84e6d19736a9624a00521
WILLIAM BOUET <william.bouet.14@gmail.com>
2023-01-29 16:45:59 +0100

    added carousel to show artworks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -10,13 +10,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
+    public const NUMBER_OF_ARTISTS_IN_FOCUS = 4;
+
     #[Route('/', name: 'app_index')]
     public function index(CategoryRepository $categoryRepository, ArtistRepository $artistRepository): Response
     {
 
         return $this->render('home/index.html.twig', [
             'categories' => $categoryRepository->findBy([], ['id' => 'ASC']),
-            'artists' => $artistRepository->findBy([], ['id' => 'ASC'], 12),
+            'artists' => $artistRepository->findBy([], ['id' => 'ASC'], self::NUMBER_OF_ARTISTS_IN_FOCUS),
         ]);
     }
 }
